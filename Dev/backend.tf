272b5118f27b3cd87ef413bd84af0d12a82aba73
Elena <elelev@gmail.com>
2026-08-10 03:10:20 -0600

    Initial project baseline

---
+++ Dev/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "bs101-dev-app-buckettt"
+    key            = "dev/terraform.tfstate"         # Use a unique path per environment
+    region         = "us-west-2"
+    #dynamodb_table = "bs101-dev-app-dbb"
+    encrypt        = true
+  }
+}
