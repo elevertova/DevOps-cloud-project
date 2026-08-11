c2ec291a42c29f499946943c60f40dc3388b9068
Elena <elelev@gmail.com>
2026-08-11 13:36:43 -0600

    Rename S3 bucket

--- UAT/backend.tf
+++ UAT/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "frhn-uat-app-bucket-temp"
+    bucket         = "frhn-uat-app-bucket"
     key            = "uat/terraform.tfstate"         # Use a unique path per environment
     region         = "us-west-2"
     dynamodb_table = "frhn-uat-app-db"
